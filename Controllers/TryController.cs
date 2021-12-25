228636294626366c2b49ab0324a03200eafdd966
fold-squirrel <20201701804@cis.asu.edu.eg>
2021-12-25 10:53:58 +0200

    Sessions almost working

--- Controllers/TryController.cs
+++ Controllers/TryController.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using dotnetCoreMVC.Models;
+using System.IO;
 
 namespace dotnetCoreMVC.Controllers;
 
@@ -12,13 +17,33 @@ public class TryController : Controller
       return View();
    }
 
+   public void loadDataFromLocalFile()
+   {
+      string[] messages = System.IO.File.ReadAllLines("./wwwroot/txtFile.txt");
+   }
+
+   public int convert_to_int(int? nullableInt)
+   {
+      if(nullableInt == null)
+         throw new ArgumentException("Session Id was null");
+      
+      int normalInt = Convert.ToInt32(nullableInt);
+      return normalInt;
+   }
+
    public ActionResult chat()
    {
+      //loadDataFromLocalFile();
+      string[] messages = System.IO.File.ReadAllLines("./wwwroot/txtFile.txt");
+
+      HttpContext.Session.SetInt32("Id", Id.num++);
+
       ChatMessage tryMsg = new ChatMessage
       {
-         msg = "this is a message shown using ajax"
+         msg = messages[convert_to_int(HttpContext.Session.GetInt32("Id"))] 
       };
 
       return Json(tryMsg);
    }
 }
+
---
+++ Models/Id.cs
@@ -0,0 +1,6 @@
+namespace dotnetCoreMVC.Models;
+
+public class Id
+{
+   public static int num { get; set; } = 0;
+}
